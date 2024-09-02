3b14450e14544e4c969af3487faeca6eda062ba8
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-09-01 22:14:08 -0300

    wip: home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,27 @@
+export default function Home() {
+    return (
+        <div>
+            <form action="">
+                <div>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <input id="task" />
+
+                    <label htmlFor="minutesAmount">durante</label>
+                    <input type="number" id="minutesAmount" />
+
+                    <span>minutos.</span>
+                </div>
+
+                <div>
+                    <span>0</span>
+                    <span>0</span>
+                    <span>:</span>
+                    <span>0</span>
+                    <span>0</span>
+                </div>
+
+                <button type="submit"></button>
+            </form>
+        </div>
+    );
+}
