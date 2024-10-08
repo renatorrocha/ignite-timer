6dfd141860af5704e915fed73f6d951a16fa6f40
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-10-08 08:54:26 -0300

    feat: history page

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 import { CyclesContext } from "../../contexts/cycles-context";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 export default function History() {
     const { cycles } = useContext(CyclesContext);
@@ -20,54 +22,38 @@ export default function History() {
                     </thead>
 
                     <tbody>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
+                        {cycles.map((cycle) => (
+                            <tr key={cycle.id}>
+                                <td>{cycle.task}</td>
+                                <td>{cycle.minutesAmount} minutos</td>
+                                <td>
+                                    {formatDistanceToNow(cycle.startDate, {
+                                        addSuffix: true,
+                                        locale: ptBR,
+                                    })}
+                                </td>
+                                <td>
+                                    {cycle.finishedDate && (
+                                        <Status statusColor="green">
+                                            Concluído
+                                        </Status>
+                                    )}
+
+                                    {cycle.interruptedDate && (
+                                        <Status statusColor="red">
+                                            Interrompido
+                                        </Status>
+                                    )}
+
+                                    {!cycle.finishedDate &&
+                                        !cycle.interruptedDate && (
+                                            <Status statusColor="yellow">
+                                                Em andamento
+                                            </Status>
+                                        )}
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </HistoryList>
